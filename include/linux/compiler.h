4cb94ced15ca15cffd4247e369e2e9f41b0d001a
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2017-08-21 18:47:32 +1000

    hack to not import gcc stuff
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -54,9 +54,9 @@ extern void __chk_io_ptr(const volatile void __iomem *);
 
 #ifdef __KERNEL__
 
-#ifdef __GNUC__
-#include <linux/compiler-gcc.h>
-#endif
+//#ifdef __GNUC__
+//#include <linux/compiler-gcc.h>
+//#endif
 
 #if defined(CC_USING_HOTPATCH) && !defined(__CHECKER__)
 #define notrace __attribute__((hotpatch(0,0)))
