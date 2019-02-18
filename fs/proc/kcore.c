c71e85a3f5fe08e4a04938c6f0becb806ed3e0e2
David Howells <dhowells@redhat.com>
2019-02-18 12:45:02 +0000

    Lock down /proc/kcore
    Disallow access to /proc/kcore when the kernel is locked down to prevent
    access to cryptographic data. This is limited to lockdown
    confidentiality mode and is still permitted in integrity mode.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Matthew Garrett <mjg59@google.com>

--- fs/proc/kcore.c
+++ fs/proc/kcore.c
@@ -545,6 +545,8 @@ out:
 
 static int open_kcore(struct inode *inode, struct file *filp)
 {
+	if (kernel_is_locked_down("/proc/kcore", LOCKDOWN_CONFIDENTIALITY))
+		return -EPERM;
 	if (!capable(CAP_SYS_RAWIO))
 		return -EPERM;
 
