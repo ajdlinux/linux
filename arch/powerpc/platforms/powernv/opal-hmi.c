e325fcd193a979654661effc1184ef75b34083b9
Andrew Donnellan <ajd@linux.ibm.com>
2020-06-25 00:03:17 +1000

    Fix opal_get_msg calls
    Signed-off-by: Andrew Donnellan <ajd@linux.ibm.com>

--- arch/powerpc/platforms/powernv/opal-hmi.c
+++ arch/powerpc/platforms/powernv/opal-hmi.c
@@ -298,7 +298,7 @@ static void hmi_event_handler(struct work_struct *work)
 
 	if (unrecoverable) {
 		/* Pull all HMI events from OPAL before we panic. */
-		while (opal_get_msg(__pa(&msg), sizeof(msg)) == OPAL_SUCCESS) {
+		while (opal_get_msg(ptr_to_opal(&msg), sizeof(msg)) == OPAL_SUCCESS) {
 			u32 type;
 
 			type = be32_to_cpu(msg.msg_type);
